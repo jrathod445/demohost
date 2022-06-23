a7549046d9c5cb5f18782a0938b1adf0523dc309
Jay  Rathod <jrathod445@rku.ac.in>
2022-06-23 09:27:08 +0530

    code updated

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 var express = require('express')
-var bodyparser = require('body-parser')
+var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 var router = require('./route')
 
-mongoose.connect("mongodb+srv://Jay:rP29B3S0EOc4bnxd@cluster0.01tdt.mongodb.net/?retryWrites=true&w=majority").then(()=>{
-    //rP29B3S0EOc4bnxd
-    console.log("DB Connected");
+var app = express();
+app.use('/api',router);
 
-    var app = express();
-    app.use(bodyparser.urlencoded({extended:false}))
-    app.use(express.json())
-    app.use('/api',router);
-
-    app.listen(process.env.PORT || 3000,()=>{
+// app.listen(3000,()=>{
+app.listen(3000,()=>{
     console.log("server started");
-    })
-
-}).catch((err)=>{
-    console.log(err)
 })
--- route.js
+++ route.js
@@ -1,11 +1,8 @@
 var express = require('express')
 var router = express.Router();
-var movie = require('./Model/movie')
 
-router.get('/movies',async(req,res)=>{
-    var imovie = await movie.find();
-    res.send(imovie)
-    // res.sendFile("index.html",{root:__dirname});
+router.get('/',(req,res)=>{
+    res.sendFile("index.html",{root:__dirname});
 })
 
 module.exports = router;
